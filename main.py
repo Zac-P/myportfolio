eb8ab9bc185bf4b6b494278b5b8b556014de9eec
Zac-P <73839860+Zac-P@users.noreply.github.com>
2023-10-24 12:58:15 -0400

    Create main.py
    new file

---
+++ main.py
@@ -0,0 +1,106 @@
+import streamlit as st
+from PIL import Image
+
+
+
+
+
+st.set_page_config(layout="wide")
+
+st.sidebar.title("Navigation")
+page = st.sidebar.selectbox("Select a Page", ["Home", "Notebooks", "Projects"])
+
+st.title("Portfolio")
+
+if page == "Home":
+   
+
+
+    def load_css():
+        with open("style.css") as f:
+            st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
+        st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+
+    def st_button(icon, url, label, iconsize):
+        if icon == 'youtube':
+            button_code = f'''
+            <p>
+                <a href={url} class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">
+                    <svg xmlns="http://www.w3.org/2000/svg" width={iconsize} height={iconsize} fill="currentColor" class="bi bi-youtube" viewBox="0 0 16 16">
+                        <path d="M8.051 1.999h.089c.822.003 4.987.033 6.11.335a2.01 2.01 0 0 1 1.415 1.42c.101.38.172.883.22 1.402l.01.104.022.26.008.104c.065.914.073 1.77.074 1.957v.075c-.001.194-.01 1.108-.082 2.06l-.008.105-.009.104c-.05.572-.124 1.14-.235 1.558a2.007 2.007 0 0 1-1.415 1.42c-1.16.312-5.569.334-6.18.335h-.142c-.309 0-1.587-.006-2.927-.052l-.17-.006-.087-.004-.171-.007-.171-.007c-1.11-.049-2.167-.128-2.654-.26a2.007 2.007 0 0 1-1.415-1.419c-.111-.417-.185-.986-.235-1.558L.09 9.82l-.008-.104A31.4 31.4 0 0 1 0 7.68v-.123c.002-.215.01-.958.064-1.778l.007-.103.003-.052.008-.104.022-.26.01-.104c.048-.519.119-1.023.22-1.402a2.007 2.007 0 0 1 1.415-1.42c.487-.13 1.544-.21 2.654-.26l.17-.007.172-.006.086-.003.171-.007A99.788 99.788 0 0 1 7.858 2h.193zM6.4 5.209v4.818l4.157-2.408L6.4 5.209z"/>
+                    </svg>  
+                    {label}
+                </a>
+            </p>'''
+
+            
+        elif icon == 'linkedin':
+            button_code = f'''
+            <p>
+                <a href={url} class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">
+                    <svg xmlns="http://www.w3.org/2000/svg" width={iconsize} height={iconsize} fill="currentColor" class="bi bi-linkedin" viewBox="0 0 16 16">
+                        <path d="M0 1.146C0 .513.526 0 1.175 0h13.65C15.474 0 16 .513 16 1.146v13.708c0 .633-.526 1.146-1.175 1.146H1.175C.526 16 0 15.487 0 14.854V1.146zm4.943 12.248V6.169H2.542v7.225h2.401zm-1.2-8.212c.837 0 1.358-.554 1.358-1.248-.015-.709-.52-1.248-1.342-1.248-.822 0-1.359.54-1.359 1.248 0 .694.521 1.248 1.327 1.248h.016zm4.908 8.212V9.359c0-.216.016-.432.08-.586.173-.431.568-.878 1.232-.878.869 0 1.216.662 1.216 1.634v3.865h2.401V9.25c0-2.22-1.184-3.252-2.764-3.252-1.274 0-1.845.7-2.165 1.193v.025h-.016a5.54 5.54 0 0 1 .016-.025V6.169h-2.4c.03.678 0 7.225 0 7.225h2.4z"/>
+                    </svg>
+                    {label}
+                </a>
+            </p>''' 
+        elif icon == 'medium':
+            button_code = f'''
+            <p>
+                <a href={url} class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">
+                    <svg xmlns="http://www.w3.org/2000/svg" width={iconsize} height={iconsize} fill="currentColor" class="bi bi-medium" viewBox="0 0 16 16">
+                        <path d="M9.025 8c0 2.485-2.02 4.5-4.513 4.5A4.506 4.506 0 0 1 0 8c0-2.486 2.02-4.5 4.512-4.5A4.506 4.506 0 0 1 9.025 8zm4.95 0c0 2.34-1.01 4.236-2.256 4.236-1.246 0-2.256-1.897-2.256-4.236 0-2.34 1.01-4.236 2.256-4.236 1.246 0 2.256 1.897 2.256 4.236zM16 8c0 2.096-.355 3.795-.794 3.795-.438 0-.793-1.7-.793-3.795 0-2.096.355-3.795.794-3.795.438 0 .793 1.699.793 3.795z"/>
+                    </svg>
+                    {label}
+                </a>
+            </p>'''
+        
+        elif icon == 'cup':
+            button_code = f'''
+            <p>
+                <a href={url} class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">
+                    <svg xmlns="http://www.w3.org/2000/svg" width={iconsize} height={iconsize} fill="currentColor" class="bi bi-cup-fill" viewBox="0 0 16 16">
+                        <path d="M1 2a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v1h.5A1.5 1.5 0 0 1 16 4.5v7a1.5 1.5 0 0 1-1.5 1.5h-.55a2.5 2.5 0 0 1-2.45 2h-8A2.5 2.5 0 0 1 1 12.5V2zm13 10h.5a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.5-.5H14v8z"/>
+                    </svg>
+                    {label}
+                </a>
+            </p>'''
+        elif icon == '':
+            button_code = f'''
+            <p>
+                <a href={url} class="btn btn-outline-primary btn-lg btn-block" type="button" aria-pressed="true">
+                    {label}
+                </a>
+            </p>'''
+        return st.markdown(button_code, unsafe_allow_html=True)
+
+    load_css()
+
+    col1, col2, col3 = st.columns(3)
+    col2.image(Image.open('8bit.png'))
+
+    st.header('Zachary Palmer')
+
+    st.info("About Me: ")
+    st.info("Data Science student at UNC Charlotte")
+
+    icon_size = 20
+
+    st_button('youtube', 'https://youtube.com', 'YouTube channel', icon_size)
+    st_button('medium', 'https://medium.com/', 'Read my Blogs', icon_size)
+    st_button('linkedin', 'https://www.linkedin.com/', 'Follow me on LinkedIn', icon_size)
+    st_button('cup', 'https://www.buymeacoffee.com/', 'Buy me a Coffee', icon_size)
+
+elif page == "Notebooks":
+    st.header("Notebooks")
+    st.write("This is the Notebook page.")
+    
+    
+    query = st.text_area("Enter SQL Query")
+    if st.button("Execute"):
+        
+        st.write("Query executed.")
+       
+elif page == "Projects":
+   st.header("Projects")
+   st.write(" ")
